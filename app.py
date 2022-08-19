6097c47b35a19318c1fa1d21476f2a9169044ee5
Mashym Ali <87118384+MashymAli@users.noreply.github.com>
2022-08-19 21:53:28 +0500

    final commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,request,render_template,send_file
+from pytube import YouTube
+from io import BytesIO
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index2.html')
+
+@app.route("/download",methods=['GET','POST'])
+def download():
+    if(request.method=='POST'):
+        buffer = BytesIO()
+        link = request.form.get('url')
+        yt = YouTube(link)
+        ys = yt.streams.get_highest_resolution()
+        ys.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name="Video - YT2Video.mp4", mimetype="video/mp4")
+    return render_template('index2.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
